520abf59909837a850f79ca24cc8cbd710a0fdf8
Bernard Bussy <brbitzbussy@gmail.com>
2020-05-01 15:07:38 +0200

    use event context

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import {Button} from '@material-ui/core'
+import {useEventContext} from 'context/event';
 
 export default function LandingPage() {
+    const eventContext = useEventContext();
+
+    console.log(eventContext);
+
     return (
         <div>
             Landing page
