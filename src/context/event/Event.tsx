db7a11fd4ce453a0986d6c193647eae897c802ed
Bernard Bussy <brbitzbussy@gmail.com>
2020-05-01 15:27:23 +0200

    starting ws connection

--- src/context/event/Event.tsx
+++ src/context/event/Event.tsx
@@ -1,16 +1,49 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import config from 'react-global-configuration';
+import io from 'socket.io-client';
+
+export enum WSStatus {
+    disconnected,
+    connecting,
+    connected,
+    error
+}
 
 interface EventContext {
-    eventContextConnected: boolean;
+    eventContextWSStatus: WSStatus;
 }
 
 const Context = React.createContext({} as EventContext);
 
 export default function EventContext({children}: { children?: React.ReactNode }) {
+    const [ws, setWS] = useState<SocketIOClient.Socket | undefined>(undefined);
+    const [wsStatus, setWSStatus] = useState<WSStatus>(WSStatus.disconnected);
+
+    const handleWSConnect = () => {
+        console.log('ws connected!')
+    };
+
+    const handleWSEvent = (data: any) => {
+        console.log('ws event!')
+    };
+
+    const handleWSDisconnect = () => {
+        console.log('ws disconnect!')
+    };
+
+    useEffect(() => {
+        const newWS = io(config.get('eventURL'));
+        newWS.on('connect', handleWSConnect);
+        newWS.on('event', handleWSEvent);
+        newWS.on('disconnect', handleWSDisconnect);
+        setWSStatus(WSStatus.connecting);
+        setWS(newWS);
+    }, [])
+
     return (
         <Context.Provider
             value={{
-                eventContextConnected: false
+                eventContextWSStatus: wsStatus
             }}
         >
             {children}
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import EventContext from 'context/event';
+import config from 'react-global-configuration';
+
+let subdomain = 'localhost';
+try {
+    subdomain = window.location.hostname.split('.')[0];
+} catch (e) {
+    console.error('error determining subdomain', e);
+}
+
+if (subdomain === 'localhost') {
+    config.set({
+        eventURL: 'ws://localhost:8080/ws',
+        authURL: 'http://localhost:8180/api',
+        roleURL: 'http://localhost:8181/api',
+        userURL: 'http://localhost:8182/api'
+    });
+} else {
+    config.set({
+        authURL: 'https://auth-ylnmede6pq-uc.a.run.app/api',
+        userURL: 'https://user-ylnmede6pq-uc.a.run.app/api',
+        roleURL: 'https://role-ylnmede6pq-uc.a.run.app/api'
+    });
+}
 
 ReactDOM.render(
     <React.StrictMode>
