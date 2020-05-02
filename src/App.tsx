33798cf4255af889eff4f1f441387f645f1ffb37
Bernard Bussy <brbitzbussy@gmail.com>
2020-05-02 15:55:23 +0200

    background style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core'
 import {Router, publicRoutes} from 'route';
+import background from 'assets/background.png'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: '100vh',
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center'
+    }
+}));
 
 function App() {
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
             <BrowserRouter>
                 <Router routes={publicRoutes}/>
             </BrowserRouter>
