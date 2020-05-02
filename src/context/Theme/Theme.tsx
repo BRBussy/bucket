1ab58de35a0970fc3e345bd385b79fecc1404935
Bernard Bussy <brbitzbussy@gmail.com>
2020-05-02 15:32:47 +0200

    starting color scheme

--- src/context/Theme/Theme.tsx
+++ src/context/Theme/Theme.tsx
@@ -6,35 +6,34 @@ export const defaultTheme = createMuiTheme({
         borderRadius: 4
     },
     palette: {
-        type: 'dark',
         primary: {
-            light: '#e95e86',
-            main: '#de1b55',
-            dark: '#a40f4b'
+            light: '#F8BBD0',
+            main: '#E91E63',
+            dark: '#C2185B'
         },
         secondary: {
-            light: '#acdcdd',
-            main: '#30b0b0',
-            dark: '#00827e',
-            contrastText: 'rgba(0, 0, 0, 0.87)'
+            light: '#80e27e',
+            main: '#4caf50',
+            dark: '#087f23',
+            contrastText: '#ffffff'
         },
         error: {
             light: '#e57373',
             main: '#f44336',
             dark: '#d32f2f',
-            contrastText: '#fff'
+            contrastText: '#ffffff'
         },
         text: {
-            primary: '#ffffff',
-            secondary: 'rgba(255, 255, 255, 0.54)',
+            primary: '#212121',
+            secondary: '#757575',
             disabled: 'rgba(255, 255, 255, 0.38)',
             hint: 'rgba(255, 255, 255, 0.38)'
         },
-        divider: 'rgba(255, 255, 255, 0.12)',
-        background: {
-            paper: '#232041',
-            default: 'rgba(18, 16, 39, 1)'
-        },
+        divider: '#BDBDBD',
+        // background: {
+        //     paper: '#232041',
+        //     default: 'rgba(18, 16, 39, 1)'
+        // },
         action: {
             active: '#fff',
             hover: 'rgba(0, 0, 0, 0.2)',
@@ -45,76 +44,7 @@ export const defaultTheme = createMuiTheme({
         }
     },
     typography: {
-        fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif',
-        h1: {
-            fontSize: 38,
-            lineHeight: '45px',
-            letterSpacing: '-0.5px'
-        },
-        h2: {
-            fontWeight: 400,
-            fontSize: 26,
-            lineHeight: '28px',
-            letterSpacing: '0px'
-        },
-        h3: {
-            fontWeight: 700,
-            fontSize: 24,
-            lineHeight: '29px',
-            letterSpacing: '0.25px'
-        },
-        h4: {
-            fontSize: 20,
-            lineHeight: '24px',
-            letterSpacing: 0
-        },
-        h5: {
-            fontSize: 16,
-            lineHeight: '21px',
-            letterSpacing: '0.25px'
-        },
-        h6: {
-            fontWeight: 600,
-            fontSize: 14,
-            lineHeight: '16px',
-            letterSpacing: '0.25px'
-        },
-        subtitle1: {
-            fontSize: 16,
-            lineHeight: '21px',
-            letterSpacing: '0.15px'
-        },
-        subtitle2: {
-            fontWeight: 400,
-            fontSize: 14,
-            lineHeight: '21px',
-            letterSpacing: '0.1px'
-        },
-        body1: {
-            fontSize: 14,
-            lineHeight: '19px',
-            letterSpacing: '0.5px'
-        },
-        body2: {
-            fontSize: 12,
-            lineHeight: '16px',
-            letterSpacing: '0.25px'
-        },
-        button: {
-            fontSize: 14,
-            lineHeight: '21px',
-            letterSpacing: '1.25px'
-        },
-        caption: {
-            fontSize: 12,
-            lineHeight: '15px',
-            letterSpacing: '0.4px'
-        },
-        overline: {
-            fontSize: 12,
-            lineHeight: '15px',
-            letterSpacing: '1px'
-        }
+        fontFamily: '"Poppins", "Helvetica", "Arial", sans-serif'
     }
 });
 
